82260c04a62f2d1ec8c4d0088b0e7e6feebd3925
pikesley <sam.pikesley@gmail.com>
2019-12-23 15:08:02 +0000

    Map Zones to IDs

---
+++ potado/lib/zone_mapper.py
@@ -0,0 +1,11 @@
+from lib.client import TadoClient
+
+
+class ZoneMapper(dict):
+    """Class to find IDs for Zones."""
+
+    def __init__(self):   # pylint: disable=W0231
+        """Construct."""
+        self.client = TadoClient()
+        for zone in self.client.zones():
+            self[zone['name']] = zone['id']
---
+++ potado/tests/unit_test_zone_mapper.py
@@ -0,0 +1,24 @@
+import json
+
+from mock import MagicMock, mock_open, patch
+
+from lib.client import TadoClient
+from lib.zone_mapper import ZoneMapper
+
+
+class TestZoneMapper:
+    """Test the ZoneMapper."""
+
+    def test_mapping(self):
+        """Test it maps."""
+        fixture = json.loads(open('tests/fixtures/api-data/zones.json').read())
+        creds_fixture = open('tests/fixtures/conf/credentials.yaml').read()
+        with patch('builtins.open', mock_open(read_data=creds_fixture)):
+            TadoClient.zones = MagicMock(return_value=fixture)
+
+            mapper = ZoneMapper()
+            assert mapper == {
+                'Bedroom': 2,
+                'Heating': 1,
+                'Kitchen': 3
+            }

5c174f3430c6d4cf6aaf7f2dec02e24f929c9be8
pikesley <sam.pikesley@gmail.com>
2019-12-23 15:05:41 +0000

    Add class to fill gaps with Ambient periods

---
+++ potado/lib/gap_filler.py
@@ -0,0 +1,60 @@
+from lib import config
+from lib.period import Period
+
+
+class GapFiller(list):
+    """Round-out a schedule to cover 24 hours."""
+
+    def __init__(self, periods, day_type):  # pylint: disable=W0231
+        """Construct."""
+        self.periods = periods
+        self.day_type = day_type
+        self.settings = config.settings()
+        self.pad()
+
+    def pad(self):
+        """Plug the gaps."""
+        reduced_periods = list(map(GapFiller.reduce, self.periods))
+        for filler in GapFiller.fill_around(reduced_periods):
+            data = {
+                'start': filler[0],
+                'end': filler[1],
+                'mode': 'ambient'
+            }
+            self.append(Period(data, self.day_type))
+
+        self.extend(self.periods)
+
+        self.sort(key=lambda period: period['start'])
+
+    @staticmethod
+    def fill_around(periods):
+        """Plug the gaps."""
+        if not periods:
+            return [('00:00', '00:00')]
+
+        filler = []
+
+        if not periods[0][0] == '00:00':
+            filler.append(('00:00', periods[0][0]))
+
+        for index, _ in enumerate(periods):
+            try:
+                now_end = periods[index][1]
+                next_start = periods[index + 1][0]
+
+                # otherwise we'll create a zero-length ambient period
+                if not now_end == next_start:
+                    filler.append((now_end, next_start))
+            except IndexError:
+                pass
+
+        if not periods[-1][1] == '00:00':
+            filler.append((periods[-1][1], '00:00'))
+
+        return filler
+
+    @staticmethod
+    def reduce(period):
+        """Reduce a period to two timestamps."""
+        return (period['start'], period['end'])
---
+++ potado/tests/unit_test_gap_filler.py
@@ -0,0 +1,99 @@
+import ruamel.yaml
+from mock import patch
+
+from lib.gap_filler import GapFiller
+
+
+@patch('lib.config.settings')
+class TestGapFiller:
+    """Test the GapFiller."""
+
+    def setup_method(self):
+        """Do some initialisation."""
+        yaml = ruamel.yaml.YAML()
+        self.fixture = yaml.load(open('tests/fixtures/conf/settings.yaml'))
+
+    def test_simplest(self, mock_settings):
+        """Test the Zero case."""
+        mock_settings.return_value = self.fixture
+        periods = []
+        day_type = 'mon-fri'
+        filler = GapFiller(periods, day_type)
+        assert filler == [
+            {
+                'dayType': 'MONDAY_TO_FRIDAY',
+                'start': '00:00',
+                'end': '00:00',
+                'setting': {
+                    'temperature': {
+                        'celsius': 3
+                    },
+                    'power': 'ON',
+                    'type': 'HEATING'
+                },
+                'geolocationOverride': False
+            }
+        ]
+
+    def test_with_one_existing_period(self, mock_settings):
+        """Test filling around one period."""
+        mock_settings.return_value = self.fixture
+        periods = [
+            {
+                'dayType': 'MONDAY_TO_FRIDAY',
+                'start': '08:00',
+                'end': '09:00',
+                'setting': {
+                    'temperature': {
+                        'celsius': 500
+                    },
+                    'power': 'ON',
+                    'type': 'HEATING'
+                },
+                'geolocationOverride': False
+            }
+        ]
+
+        day_type = 'mon-fri'
+        filler = GapFiller(periods, day_type)
+        assert filler == [
+            {
+                'dayType': 'MONDAY_TO_FRIDAY',
+                'start': '00:00',
+                'end': '08:00',
+                'setting': {
+                    'temperature': {
+                        'celsius': 3
+                    },
+                    'power': 'ON',
+                    'type': 'HEATING'
+                },
+                'geolocationOverride': False
+            },
+            {
+                'dayType': 'MONDAY_TO_FRIDAY',
+                'start': '08:00',
+                'end': '09:00',
+                'setting': {
+                    'temperature': {
+                        'celsius': 500
+                    },
+                    'power': 'ON',
+                    'type': 'HEATING'
+                },
+                'geolocationOverride': False
+            },
+            {
+                'dayType': 'MONDAY_TO_FRIDAY',
+                'start': '09:00',
+                'end': '00:00',
+                'setting': {
+                    'temperature': {
+                        'celsius': 3
+                    },
+                    'power': 'ON',
+                    'type': 'HEATING'
+                },
+                'geolocationOverride': False
+            }
+        ]

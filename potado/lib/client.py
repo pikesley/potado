55a4f4fdc3c64f8b5b5dd2f1078547977b2d3897
pikesley <sam.pikesley@gmail.com>
2019-12-23 15:03:54 +0000

    Refine the Client

--- potado/lib/client.py
+++ potado/lib/client.py
@@ -1,7 +1,9 @@
 import json
 
 import requests
-import yaml
+
+import lib.utils as utils
+from lib import config
 
 
 class TadoClient:
@@ -9,9 +11,9 @@ class TadoClient:
 
     def __init__(self):
         """Construct."""
-        self.creds = yaml.safe_load(open('conf/credentials.yaml'))
+        self.credentials = config.credentials()
+        self.base_url = 'https://my.tado.com/api/v2/homes'
 
-    @property
     def bearer_token(self):
         """Get the bearer token."""
         auth_url = "https://auth.tado.com/oauth/token"
@@ -19,9 +21,9 @@ class TadoClient:
             "client_id": "tado-web-app",
             "grant_type": "password",
             "scope": "home.user",
-            "username": self.creds['username'],
-            "password": self.creds['password'],
-            "client_secret": self.creds['client_secret']
+            "username": self.credentials['username'],
+            "password": self.credentials['password'],
+            "client_secret": self.credentials['client_secret']
         }
 
         response = requests.post(auth_url, payload)
@@ -33,7 +35,7 @@ class TadoClient:
     def headers(self):
         """Define the HTTP headers."""
         return {
-            "Authorization": f"Bearer {self.bearer_token}",
+            "Authorization": f"Bearer {self.bearer_token()}",
             "Content-type": "application/json"
         }
 
@@ -46,16 +48,43 @@ class TadoClient:
 
         return body
 
-    @property
     def home_id(self):
         """Get the Home ID."""
         return self.my_data['homeId']
 
-    @property
     def zones(self):
         """Get the Zones data."""
-        url = f'https://my.tado.com/api/v2/homes/{self.home_id}/zones'
+        url = f'{self.base_url}/{self.home_id()}/zones'
         response = requests.get(url, headers=self.headers)
         body = json.loads(response.text)
 
         return body
+
+    def put_schedule(self, url_fragment, day_type, data):
+        """Put the schedule data for a zone."""
+        url = '{0}/{1}/{2}/{3}'.format(
+            self.base_url,
+            self.home_id(),
+            url_fragment,
+            utils.day_type_for(day_type)
+        )
+        response = requests.put(
+            url,
+            data=json.dumps(data),
+            headers=self.headers
+        )
+
+        return response
+
+    def put_three_days(self, zone_id):
+        """Set everything  to the THREE_DAY timetable."""
+        url = ('{0}/{1}/zones/{2}/schedule/activeTimetable').format(
+            self.base_url, self.home_id(), zone_id
+        )
+
+        response = requests.put(
+            url,
+            data=json.dumps({"id": 1}),
+            headers=self.headers
+        )
+        return response
